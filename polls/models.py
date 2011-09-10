f2531ca5f998e90093bc34e7a8a23c88a39ef37d
Alizon Elliott <alizon.elliott@me.com>
2011-09-10 14:23:31 -0700

    Updated the fields.

--- polls/models.py
+++ polls/models.py
@@ -3,12 +3,20 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
+import datetime
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True,null=True)
+    def __unicode__(self):
+    	return self.question
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()    	
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+    def __unicode__(self):
+    	return self.choice
