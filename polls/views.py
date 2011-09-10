50951bba4465d938828dc8a871e0ca7a015d0836
Alizon Elliott <alizon.elliott@me.com>
2011-09-10 14:45:42 -0700

    Changed response handler and a def

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,20 @@
 # Create your views here.
 
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from polls.models import Poll
 
 def index(request):
-	return HttpResponse("Hello, World!  You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render_to_response('polls/index.html', context)
+    
+# def index(request):
+#    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#   output = ', '.join([p.question for p in latest_poll_list])
+#    return HttpResponse(output)
+
+# def index(request):
+#	return HttpResponse("Hello, World!  You're at the poll index.")
 	
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % (poll_id,))
